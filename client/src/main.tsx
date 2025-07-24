496c1c37453da5ac50599f8c6fb622890ebb1409
Eknath Changade <Eknathchangade833@gmail.com>
2025-07-24 22:30:20 +0530

    hello

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,9 @@
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
+import "./index.css"; // Optional
 
-createRoot(document.getElementById("root")!).render(<App/>);
+const root = document.getElementById("root");
+if (root) {
+  ReactDOM.createRoot(root).render(<App />);
+}
